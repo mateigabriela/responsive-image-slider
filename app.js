69117913b0bb861c3569715c74682a194a5b7dc0
mateigabriela <mateigabriela202@gmail.com>
2025-03-19 10:37:21 +0000

    main page without the correct text, animated button and you need to fic the indicator, it s a little too big on the left

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
+const slideTabs = document.querySelectorAll(".slide-tab");
+const slideIndicator = document.querySelector(".slide-indicator");
+const slideControls = document.querySelector(".slide-controls");
+const updateIndicator = (tab,index) => {
+    slideIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
+    slideIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+    const scrollLeft = slideTabs[index].offsetLeft - slideControls.offsetWidth /2 + slideTabs[index].offsetWidth/2;
+    slideControls.scrollTo({left: scrollLeft, behavior: "smooth"});
+};
+
 const swiper = new Swiper(".slide-container", {
     effect: "fade",
     speed: 1300,
     // autoplay: {delay: 400}
+    navigation: {
+        prevEl: "#slide-prev",
+        nextEl: "#slide-next"
+    },
+    on: {
+        slideChange: () => {
+            const currentTabIndex = [...slideTabs].indexOf(slideTabs[swiper.activeIndex]);
+            updateIndicator(slideTabs[swiper.activeIndex],currentTabIndex);
+        }
+    }
 });
+
+//updateaza slide-ul in functie de tab-ul pe care dam click
+slideTabs.forEach((tab,index) => { //iteram prin fiecare element din slideTabs(tab), avand si indexul fiecaruia
+    tab.addEventListener("click", ()=>{ 
+        swiper.slideTo(index); //cand facem click pe un tab, ne va duce la poza cu index-u; corespunzator tab-ului cu ajutorul functiei swiper
+        updateIndicator(tab,index);
+    });
+});
+
+
+updateIndicator(slideTabs[0], 0);
+window.addEventListener("resize", () => updateIndicator(slideTabs[swiper.activeIndex], 0));
+
+
 
